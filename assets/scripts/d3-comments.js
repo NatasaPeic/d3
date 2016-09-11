82b7ea61315b9165539437f5212ba7c4e46a7afa
NatasaPeic <peic.natasa@gmail.com>
2016-09-11 16:07:58 -0400

    Add comments and presentation

--- assets/scripts/d3-comments.js
+++ assets/scripts/d3-comments.js
@@ -3,94 +3,122 @@
 
 
 // Set the dimensions of the canvas / graph
+// margin convention specifies what margin the inner drawing space will have
 let margin = {top: 30, right: 50, bottom: 100, left: 50},
     width = 800 - margin.left - margin.right, // width = 800 (svg container)-100, for the graph
     height = 600 - margin.top - margin.bottom; // height = 600 (svg container)- 130 for the graph
 
 
 // Parse the date / time
+// define date parsing function
+// Takes string and converts it into js date object
+// This takes in a string that has a date as a year
 let parseDate = d3.time.format("%Y").parse;
 
 
 
 // Set the ranges
-// time scale funcion for x axis data. it creates scaling function where range goes from 0 to the width of inner drawing space.
-// scale linear function for y axis. it creates scaling function where the range goes from the height of the inner drawing space to 0.
+// define x scaling as well as range of the function
+// time scale funcion for x axis data. it creates scaling function where range goes from 0 to the width of inner drawing space. we set domain later when we load the data
+// scale linear function for y axis. it creates scaling function where the range goes from the height of the inner drawing space to 0. y axis matches number of people and it is linear. it is backwards because it inverts the svg coordinate space along y axis. origin point will be at the bottom left instead of top left.
 let x = d3.time.scale().range([0, width]);
 let y = d3.scale.linear().range([height, 0]);
 
 
 
-// create x, y axis functions
+// create x, y axis functions. we pass in x scale function that was created earlier
 // ticks sets data points
+// will be called once we have range, domain and scale functions set
 let xAxis = d3.svg.axis().scale(x)
     .orient("bottom").ticks(10);
+    // it appears on the left
 let yAxis = d3.svg.axis().scale(y)
     .orient("left").ticks(10);
 
 
 
-
 // Define the line
-// it complies the date and the rate in our dataset which means that x values access the date from the data passed through anonymous function
+// define d3 path generate function
+// for x and y the code defines accessor functions. it complies the date and the rate in our dataset which means that x values access the date from the data passed through anonymous function
 let line = d3.svg.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.rate); });
 
 
 
-
-
-
-// creates svg container and inner drawing space
+// Adds the svg canvas
+// define svg container and inner drawing space
+// Select div tag and appends it
 let svg = d3.select("#cdc-1")
         .append("svg")
         // it defines width and height attribues of the svg container in terms of inner drawing space width and height and relevant margin
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         // append svg group element which will be inner drawing space
+        // g stands for svg group that contains visualization
         .append("g")
+       // inner drawing space is transformed translated to the down by the relevant margins
        // the element is moved by a relative value in the x,y direction.
         .attr("transform",
               "translate(" + margin.left + "," + margin.top + ")");
 
 
 
-
+// up until now we have created code that we are going to use, now we need to call it
 
 // Get the data
-// d3.csv function calls callback function and passes two arguments, error and data, callback in anonymous function
+// building of chart happens inside of callback function
+// code where d3 does  XHR call to the server to get the death-rate.csv file
+// once server responds with the file, d3.csv function calls callback function and passes two arguments, error and data, callback in anonymous function
 let chart1 = d3.csv("death-rate.csv", function(error, data) {
-  // it iterates through array of javascript objects
-  // it redefines the values
+  // it iterates throuhg array of javascript objects
+  // it redefines the values of the same key of the same object
+  // it applies anonymous function to each element of the array.
     data.forEach(function(d) {
-      // converts date string into javascript object
+      // it does two things. to convert data into more usable type of data. first it converts date string into javascript object
 		d.date = parseDate(d.date);
     // converts string rate to a number
 		d.rate = +d.rate;
     });
 
+  //  we set domain later when we load the data
+    // Scale the range of the data to fit avaiable space
 
-    // once data is loaded, we need to set domain
     // domain rep­re­sents the bound­aries within which this data lies
+    // domain for the x axis values will be determined by the d3.extent function which in turn is acting on a separate function which looks through all the 'date' values that occur in the 'data' array. In thins case the .extent function returns the minimum and maximum value in the given array.
 
-    // domain for the x axis values will be determined by the d3.extent function which looks through all the 'date' values that occur in the 'data' array.
+    // because the range of values desired on the y axis goes from 0 to the maximum in the data range, that's exactly what we tell D3. The '0' in the .domain function is the starting point and the finishing point is found by employing a separate function that sorts through all the 'rate' values in the 'data' array and returns the largest one. Therefore the domain is from 0 to 636.23.
 
-
-    // extend returns array containg the minimum and maximum values in the given array
+    // when we have data we can set domain for x, y scale function
+    // extend returns array containg the minimum and maximum dates, it uses anonymous function to get the date of the data object
     x.domain(d3.extent(data, function(d) { return d.date; }));
-
+    // y.domain([0, d3.max(data, function(d) { return d.rate; })]);
     y.domain(d3.extent(data, function(d) { return d.rate; }));
 
 
 
+    // Nest the entries by symbol
+    // used for large group of data, grouping by symbols (leading cause)
+    let dataNest = d3.nest()
+        // key is individual step that data goes through
+        // data is rearanged
+        .key(function(d) {return d.symbol;})
+        // acitvate the data, data is grouped by symbol (leading cause)
+        .entries(data);
+
+
+    // Constructs a new ordinal scale with a range of ten categorical colors
+    let color = d3.scale.category10();  // set the color scale
+
 
     // Add the X Axis
+    // call d3 operator for the x axis
     // it appends the svg group elements to hold the x axis
     svg.append("g")
         // group is given the class "x axis"
         .attr("class", "x axis")
         // transform translated by the height of the inner drawing space
+        // it moves to the bottom of the inner drawing space
         .attr("transform", "translate(0," + height + ")")
         // function is called because we have x scaling function, domain and range
         .call(xAxis);
@@ -112,36 +140,22 @@ let chart1 = d3.csv("death-rate.csv", function(error, data) {
 
 
 
-
-    // Nest the entries by symbol
-    // used for large group of data, grouping by symbols (leading cause)
-    let dataNest = d3.nest()
-        // data is rearanged
-        .key(function(d) {return d.symbol;})
-        // acitvate the data, data is grouped by symbol (leading cause)
-        .entries(data);
-
-
-    // Constructs a new ordinal scale with a range of ten categorical colors
-    let color = d3.scale.category10();  // set the color scale
-
-
-
-
-
     // Loop through each symbol / key
     dataNest.forEach(function(d) {
-         // path will be generated by d3 path generator functionality
+          // create line using d3 path generator
+          // define path
          svg.append("path")
-            // add class of line to style it
             .attr("class", "line")
             .style("stroke", function() { // Add dynamically
                 return d.color = color(d.key); })
-            // add attribute d it will take data and generate path
+             // add attribute d it will take data and generate path
             .attr("d", line(d.values));
     });
-});
 
+
+
+
+});
 
 
 d3.select("#cdc-1").append("h4").html("Looking at this plot, we can see that leading causes of deaths 100 years ago were influenza, stroke and heart disease. Starting from 1950. we can see the death rates are decreasing for all causes of deaths except for one, and that is cancer.")
--- assets/scripts/d3-json.js
+++ assets/scripts/d3-json.js
@@ -4,9 +4,10 @@
 // it just running d3.nest to represent the data
 
      function render(data){
-
+       // it calls  d3.nest
        let nested = d3.nest()
        // d.symbol loops through all symbols out of data
+       // d is row object for induvidual row
          .key(function (d){ return d.symbol; })
          .entries(data);
 
@@ -20,4 +21,5 @@
        return d;
      }
 
+ // when csv is loaded it gets passed to render function
      d3.csv("death-rate-reduced.csv", type, render);
